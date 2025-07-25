9c01e0ebaa5a8d5863a5cfe9a865d94603a761a8
재선 <ddorri83@naver.com>
2025-07-26 01:59:13 +0900

    day21_New페이지_UI,기능구현

--- section12/src/pages/Edit.jsx
+++ section12/src/pages/Edit.jsx
@@ -1,10 +1,35 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
 
 const Edit = () => {
     const params = useParams();
+    const nav = useNavigate();
+    const {onDelete} = useContext(DiaryDispatchContext)
+
+    const onClickDelete = () => {
+        if (
+            window.confirm("일기를 정말 삭제할까요? 다시 복구되지 않아요! ")
+        ){
+            //일기 삭제 로직
+            onDelete(params.id);
+            nav('/',{replace: true})
+        }
+        
+    }
 
     return (
-        <div>{params.id}번 일기입니다.</div>
+        <div>
+            <Header 
+                title={"일기 수정하기"}
+                leftChild={<Button onClick={()=>nav(-1)} text={"< 뒤로 가기"}/>}
+                rightChid={<Button onClick={onClickDelete} text={"삭제하기"} type={"NEGATIVE"}/>}
+            />
+            <Editor />
+        </div>
     )
 }
 
