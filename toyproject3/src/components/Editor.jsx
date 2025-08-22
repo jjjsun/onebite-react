71b033e6e4a6dd7298796e50242ce0e3d4294f4c
재선 <ddorri83@naver.com>
2025-08-22 14:57:12 +0900

    일기장토이프로젝트

---
+++ toyproject3/src/components/Editor.jsx
@@ -0,0 +1,80 @@
+import './Editor.css'
+import EmotionItem from "../components/EmotionItem";
+import Button from './Button';
+import { getEmotionImage } from '../util/get-emotion-image';
+import { emotionList } from '../util/constant';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { getStringdate } from '../util/get-stringed-date';
+
+const Editor = ({onSubmit, initData}) => {
+
+    const nav = useNavigate();
+
+    const [input, setInput] = useState({
+        createdDate : new Date(),
+        emoitonId: 3,
+        content : "",
+    });
+
+
+    useEffect(()=>{
+        if(initData){
+            setInput({...initData, createdDate: new Date(Number(initData.createdDate))})
+        }
+    },[initData])
+
+
+    const onChangeInput = (e) => {
+
+        let name = e.target.name;
+        let value = e.target.value;
+
+        if(name === 'createdDate'){
+            value = new Date(value);
+        }
+
+        setInput({...input, [name]: value})
+    }
+
+    const onClickSubmitButton = () => {
+        onSubmit(input);
+    }
+
+    return (
+        <div className='Editor'>
+            <section className='date_section'>
+                <h4>오늘의 날짜</h4>
+                <input name="createdDate" onChange={onChangeInput} value={getStringdate(input.createdDate)} type='date'/>
+            </section>
+            <section className='emotion_section'>
+                <h4>오늘의 감정</h4>
+                <div className='emotion_list_wrapper'>
+                    {emotionList.map((item)=>  
+                        <EmotionItem 
+                            onClick={()=>
+                                onChangeInput({
+                                    target: {
+                                        name: "emotionId",
+                                        value: item.emotionId,
+                                    },
+                                })
+                            }
+                            key={item.emotionId} {...item} isSelected={item.emotionId === input.emotionId}
+                        />
+                    )}
+                </div>
+            </section>
+            <section className='content_section'>
+                <h4>오늘의 일기</h4>
+                <textarea name="content" value={input.content} onChange={onChangeInput} placeholder='오늘은 어땠나요?'/>
+            </section>
+            <section className='button_section'>
+                <Button onClick={()=>nav(-1)} text={"취소하기"}/>
+                <Button onClick={onClickSubmitButton} text={"작성완료"} type={"POSITIVE"}/>
+            </section>
+        </div>
+    )
+}
+
+export default Editor
---
+++ toyproject3/src/pages/Edit.jsx
@@ -0,0 +1,43 @@
+import { useParams, useNavigate } from "react-router-dom";
+import Header from "../components/Header";
+import Editor from "../components/Editor";
+import Button from "../components/Button";
+import { useContext, useEffect, useState } from "react";
+import { DiaryDispatchContext } from "../App";
+import { DiaryStateContext } from "../App";
+import useDiary from "../hooks/useDiary";
+
+const Edit = () => {
+    const params = useParams();
+    const nav = useNavigate();
+    const {onDelete, onUpdate} = useContext(DiaryDispatchContext);
+    
+
+    const curDiaryItem = useDiary(params.id);
+
+    const onClickDelete = () => {
+        if(
+            window.confirm("일기를 진짜 삭제할까요? 다시 복구되지 않습니다!")
+        ){
+            onDelete(params.id);
+            nav('/',{replace: true})
+        }
+    }
+
+    const onSubmit = (input) => {
+        if(window.confirm("일기를 정말 수정할까요?")){
+            onUpdate(params.id, input.createdDate.getTime(), input.emotionId, input.content);
+            nav('/',{replace: true});
+        }
+        
+    }
+
+    return (
+        <div>
+            <Header title={"일기 수정하기"} leftChild={<Button onClick={()=>nav(-1)} text={"< 뒤로 가기"}/>} rightChild={<Button onClick={onClickDelete} text={"삭제하기"} type={"NEGATIVE"}/>}/>
+            <Editor initData={curDiaryItem} onSubmit={onSubmit}/>
+        </div>
+    )
+}
+
+export default Edit;
